b0b124b760255c85c6c4d840cdd514db6c1962c1
MounirBHK <mounir.bhk@gmail.com>
2024-01-29 09:39:34 -0500

    premier test cypress

--- client/src/components/products/ProductRow.jsx
+++ client/src/components/products/ProductRow.jsx
@@ -4,9 +4,19 @@ export default function ProductRow({ product }) {
 
     const style = product ? undefined : { color: 'red' }
 
+    // Condition pour afficher le lien vers apple.com si le nom du produit est "Apple"
+    const link = product && product.name.toLowerCase() === 'apple' ? (
+        <a href="https://www.apple.com" target="_blank" rel="noopener noreferrer">
+            {product.name}
+        </a>
+    ) : (
+        product.name
+    );
+
     return (
         <tr>
-            <td style={style} >{product.name}</td>
+            {/* <td style={style} >{product.name}</td> */}
+            <td style={style} >{link}</td>
             <td>{product.price}</td>
         </tr>
     )
--- server/server.js
+++ server/server.js
@@ -5,18 +5,22 @@ const PORT = 5000;
 
 app.use(cors());
 
-app.get('/api', (req, res) => {
-    res.json({
-        PRODUCTS: [
-            { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-            { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-            { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-            { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-            { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-            { category: "Vegetables", price: "$1", stocked: true, name: "Péas" }
-        ]
-    });
-});
+// app.get('/api', (req, res) => {
+//     res.json({
+//         PRODUCTS: [
+//             { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+//             { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+//             { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+//             { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+//             { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+//             { category: "Vegetables", price: "$1", stocked: true, name: "Péas" }
+//         ]
+//     });
+// });
+
+app.get('/', (req, res)=>{
+    res.send('true');
+})
 
 
 app.listen(PORT, () => {
